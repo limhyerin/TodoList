c1069614f6e3472b1b4440f841a6be13c2b37b1f
limhyerin <rin091345@gmail.com>
2024-01-23 10:17:27 +0900

    세부 수정

--- src/App.jsx
+++ src/App.jsx
@@ -3,19 +3,12 @@ import "./App.css";
 import Todo from "components/Todo";
 import Button from "components/Button";
 
-// 1. **JSX 문법**이란 무엇일까요?
-// 2. 사용자가 입력하는 값, 또는 이미 입력된 값, 투두의 타이들과 같은 **애플리케이션의 상태를 관리하기 위해 리액트의 어떤 기능을 사용하셨나요**?
-// 3. 애플리케이션의 **상태 값들을 컴포넌트 간 어떤 방식으로 공유하셨나요**?
-// 4. 기능 구현을 위해 **불변성 유지가** 필요한 부분이 있었다면 하나만 설명해 주세요.
-// 5. 반복되는 컴포넌트를 파악하고 재사용할 수 있는 **컴포넌트로 분리해 보셨나요?** 그렇다면 **어떠한 이점이 있었나요?**
-
 // 사용한 hook은 오직 useState
 // 기능 구현을 위해 생성한 함수는 2개 입니다. `onChangeHandler` , `onSubmitHandler`
 // 사용한 javascript 내장 메서드는 `map`, `filter` 입니다.
 // todo의 initial state는 `{id: 0, title: “”, body: “”, isDone: false}` 입니다.
 
 const App = () => {
-  // 구조분해할당
   const [todo, setTodo] = useState([]);
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
@@ -30,9 +23,6 @@ const App = () => {
 
   // TODO LIST 작성하고 추가 버튼 클릭시 내용 추가
   const clickAddHandler = (event) => {
-    // 1. 새로운 형태의 이놈을 만든다
-    // 이놈 : {id:1, age:30, name:"송중기"},
-    // 2. 이놈을 배열에 더한다.
     if(title && body) {
       const newTodo = {
         id: todo.length + 1,
@@ -43,26 +33,23 @@ const App = () => {
       // todo list를 입력한 후 입력하는 칸 지우기
       setTitle('');
       setBody('');
-    } else if (!body || !title) { // 제목이나 내용이 입력되지 않았을 시 alert 창 띄우기
-      alert('내용을 입력해주세요');
     } else { // 아무것도 입력되지 않았다면 입력이 없다는 alert 창 띄우기
-      alert('입력이 제대로 되지 않았습니다');
+      alert('입력이 없습니다');
     }
   }
 
   // 삭제 버튼 클릭(x)
   const clickDelHandler = (id) => {
-    // 만약 송중기가 들어가 있는 값을 뺀다면?
     const newTodo = todo.filter(todo => todo.id !== id);
     /* 화살표 함수가 아닌 function함수로 하면 
-    const newUsers = users.filter(function(user) {
-      return user.id !== id;
+    const newTodo = todo.filter(function(todo) {
+      return todo.id !== id;
     }
     */
     setTodo(newTodo);
   }
 
-  // 완료시 Done으로 이동
+  // 완료시 Done으로 이동, isDone true
   const clickDoneHandler = (id) => {
     const newTodo = todo.map(function (todo) {
       if(todo.id === id) {
@@ -73,8 +60,7 @@ const App = () => {
     setTodo(newTodo);
   }
 
-  // 취소버튼 클릭시 다시 Working으로 옮겨지도록
-  // 완료시 Done으로 이동
+  // 취소버튼 클릭시 다시 Working으로 옮겨지도록 isDone false값 주기
   const clickCancelHandler = (id) => {
     const newTodo = todo.map(function (todo) {
       if(todo.id === id) {
